88dcd262a13f6349d29f57885217467f2ff84685
nguye <nguyenv2@outlook.com>
2018-10-09 11:09:07 -0700

    README added

--- About-Me-Quiz/About-Me-Quiz/Program.cs
+++ About-Me-Quiz/About-Me-Quiz/Program.cs
@@ -8,9 +8,11 @@ namespace About_Me_Quiz
     {
       Console.WriteLine("Hello World!");
       Console.WriteLine("My Name is Vinh");
-      if (Game() == true)
+      //Asks if user wants to play. Exits out of reply is no.
+      if (PlayGame())
       {
         Console.WriteLine("Great! Let's get started");
+        //Calls up overall method to run game. Also keeps track of number of right answers.
         Counter();
         Console.ReadKey();
       }
@@ -28,14 +30,17 @@ namespace About_Me_Quiz
       string questionFour = "What branch of the military did I serve in?";
       string questionFive = "How much money have I lost in Vegas?";
 
+      //keeps track of number of right answers by summing up return from Questions() method. Integer input is to assign id to each question. It's not necessarily needed but easier to deal with using an integer.
       int count = Questions(1, questionOne) + Questions(2, questionTwo) + Questions(3, questionThree) + Questions(4, questionFour) +Questions(5, questionFive);
 
       Console.WriteLine($"You got {count} questions correct out of 5.");
     }
 
-    static bool Game()
+    //method asks if user wants to play game and returns true or false based on yes or no input
+    static bool PlayGame()
     {
       string answer;
+      //the loop with the catch exception prevents user from submitting empty input and will continue to ask until user inputs something. Same algorithm is used in Questions() method
       while (true)
       {
         try
@@ -68,10 +73,12 @@ namespace About_Me_Quiz
     static int Questions(int questionNumber, string questionContent)
     {
       string output;
+      //samething in PlayGame() method
       while (true)
         try
         {
           Console.WriteLine(questionContent);
+          //compares user's reply to stored answer
           string answer = Console.ReadLine();
           output = CheckAnswer(answer, questionNumber);
           break;
@@ -81,6 +88,7 @@ namespace About_Me_Quiz
           Console.WriteLine(e.Message);
         }
       Console.WriteLine(output);
+      //returns 1 point if input is correct for summing.
       if (output == "Correct")
       {
         return 1;
@@ -93,10 +101,12 @@ namespace About_Me_Quiz
     
     static string CheckAnswer(string answer, int questionNumber)
     {
+      //if user has no input, throws exception to parent methods
       if (answer.Length < 1)
       {
         throw (new Exception("You Didn't Answer the Question!"));
       }
+      //matches user input to correct answer and returns either "Correct" or "Incorrect"
       else
       {
         if (answer.ToLower() == "vietnam" && questionNumber == 1)
@@ -125,7 +135,5 @@ namespace About_Me_Quiz
         }
       }
     }
-
-
   }
 }
